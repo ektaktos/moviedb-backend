3b536a91beabeeafe5247796d5a5d5240745f366
Wale Timothy <awtim01@gmail.com>
2020-10-26 18:22:47 +0100

    added root file

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const config = require('./config');
+const routes = require('./routes/index');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(routes);
+app.use(cors());
+
+app.use(function(req,res){
+  res.status(404).send({message: 'Path not found'});
+});
+
+app.listen(config.port, () =>{
+  console.log(`Server running at ${config.port}`);
+})
+
