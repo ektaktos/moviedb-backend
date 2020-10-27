7f9eaf166aec570fb5ed09910f7a2002bf8840f7
Wale Timothy <awtim01@gmail.com>
2020-10-27 23:29:15 +0100

    updated cors

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const routes = require('./routes/index');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/',routes);
 app.use(cors());
+app.use('/api/',routes);
 
 app.use(function(req,res){
   res.status(404).send({message: 'Path not found'});
