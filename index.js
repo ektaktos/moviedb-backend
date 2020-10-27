1bcd2159c2d22dd1f91beb11a943783893279162
Wale Timothy <awtim01@gmail.com>
2020-10-27 23:08:43 +0100

    added api suffix to URIs

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const routes = require('./routes/index');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(routes);
+app.use('/api/',routes);
 app.use(cors());
 
 app.use(function(req,res){
